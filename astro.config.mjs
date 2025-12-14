1aca6ea053b4714254898e10bd59babc260cd4de
sierrapablo <sierrapablo@users.noreply.github.com>
2025-12-14 21:25:23 +0000

    chore: format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 // @ts-check
-import {
-    defineConfig
-} from "astro/config";
-import {
-    portfolioTheme
-} from "./theme.mjs";
+import { defineConfig } from "astro/config";
+import { portfolioTheme } from "./theme.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    site: "https://www.sierrapablo.dev",
-    trailingSlash: "always",
-    markdown: {
-        shikiConfig: {
-            theme: portfolioTheme,
-        },
+  output: "static",
+  site: "https://www.sierrapablo.dev",
+  trailingSlash: "always",
+  markdown: {
+    shikiConfig: {
+      theme: portfolioTheme,
     },
+  },
 });
