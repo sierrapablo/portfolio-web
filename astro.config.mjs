a62935a8d744edd0b22ed5133dbd5c009b844062
sierrapablo <sierrapablo@users.noreply.github.com>
2025-12-26 12:48:13 +0000

    chore: format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
-import {
-    defineConfig
-} from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    site: "https://www.sierrapablo.dev"
+  output: "static",
+  site: "https://www.sierrapablo.dev",
 });
