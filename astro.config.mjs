036656d4deab1eb69c4a92ea70aa0b39832f90b8
Pablo Sierra Lorente <pablosierra@flit2go.com>
2025-12-26 13:45:59 +0100

    chore: remove commented imports and implementations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,9 @@
 import {
     defineConfig
 } from "astro/config";
-// import { portfolioTheme } from "./theme.mjs";
 
 // https://astro.build/config
 export default defineConfig({
     output: "static",
-    site: "https://www.sierrapablo.dev",
-    // trailingSlash: "always",
-    /**
-    markdown: {
-      shikiConfig: {
-        theme: portfolioTheme,
-      },
-    },
-    */
+    site: "https://www.sierrapablo.dev"
 });
