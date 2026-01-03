8012e1d7b9a9f482f3d6b0db4abe73167c619348
Pablo Sierra Lorente <pablosierra@flit2go.com>
2026-01-03 23:48:13 +0100

    feat: add node adapter for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import {
+    defineConfig
+} from "astro/config";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  site: "https://www.sierrapablo.dev",
+    output: "server",
+    adapter: node({
+        mode: "standalone",
+    }),
+    site: "https://www.sierrapablo.dev",
 });
