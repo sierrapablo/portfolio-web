4e5404432350f39b9aa79f3ff45dceda1ff3147c
Pablo Sierra Lorente <pablosierra@flit2go.com>
2026-01-03 22:08:21 +0100

    fix: remove redundant try-catch block and avoid using ternary operator

--- src/lib/getContributions.ts
+++ src/lib/getContributions.ts
@@ -4,20 +4,21 @@ import { GH_STATISTICS_API_ENDPOINT, GH_USERNAME } from '@/data/properties.json'
 export async function getContributions(
   year: string,
 ): Promise<{ days: ContributionDay[]; total: number } | null> {
-  try {
-    const response = await fetch(
-      `${GH_STATISTICS_API_ENDPOINT}${GH_USERNAME}?y=${!year ? 'last' : year}`,
-    );
-    if (!response.ok) {
-      return null;
-    }
-    const data: GitHubData = await response.json();
 
-    return {
-      days: data.contributions,
-      total: Object.values(data.total)[0] || 0,
-    };
-  } catch (e) {
+  if (!year) {
+    year = 'last';
+  }
+
+  const response = await fetch(
+    `${GH_STATISTICS_API_ENDPOINT}${GH_USERNAME}?y=${year}`,
+  );
+  if (!response.ok) {
     return null;
   }
+  const data: GitHubData = await response.json();
+
+  return {
+    days: data.contributions,
+    total: Object.values(data.total)[0] || 0,
+  };
 }
