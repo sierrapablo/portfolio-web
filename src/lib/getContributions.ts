a6e366f8b30dcf16d1f9996d87800625f93337c0
Jenkins CI <jenkins[bot]@noreply.jenkins.io>
2026-01-03 20:54:51 +0000

    chore: format code

--- src/lib/getContributions.ts
+++ src/lib/getContributions.ts
@@ -1,17 +1,19 @@
 import type { ContributionDay, GitHubData } from '@/types/statistics';
 import { GH_STATISTICS_API_ENDPOINT } from '@/data/properties.json';
 
-export async function getContributions(year: string): Promise<{ days: ContributionDay[], total: number } | null> {
+export async function getContributions(
+  year: string,
+): Promise<{ days: ContributionDay[]; total: number } | null> {
   try {
     const response = await fetch(`${GH_STATISTICS_API_ENDPOINT}${!year ? 'last' : year}`);
     if (!response.ok) {
-        return null;
+      return null;
     }
     const data: GitHubData = await response.json();
 
     return {
-        days: data.contributions,
-        total: Object.values(data.total)[0] || 0
+      days: data.contributions,
+      total: Object.values(data.total)[0] || 0,
     };
   } catch (e) {
     return null;
