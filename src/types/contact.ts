a70c1dda3c70a5a3c4537a7da8d274b647465969
Pablo Sierra Lorente <pablosierra@flit2go.com>
2026-01-05 02:06:26 +0100

    feat: add contact form types for send data and receive a response

---
+++ src/types/contact.ts
@@ -0,0 +1,13 @@
+export interface ContactFormData {
+    name: string;
+    email: string;
+    subject: string;
+    message: string;
+    phone?: string;
+    whatsapp: boolean;
+}
+
+export interface ContactFormResponse {
+    success: boolean;
+    message: string;
+}
